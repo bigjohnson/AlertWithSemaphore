c3afa8f7308a4bae56e8f6563e4a816d4d6dc852
Alberto Panu <alberto@panu.it>
2022-06-27 13:06:33 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 # the directory "/home/user/crons/" must be present
 SEMAFORO=/home/user/crons/test.www.github.com.SEMAFORO
-if curl -m 30 -s https://www.github.com/ > /dev/null
+# Check the internmet connectivity
+if ping -c 1 www.google.it &> /dev/null || ping -c 1 www.github.com &> /dev/null || ping -c 1 www.facebook.com &> /dev/null
 then
-        # Code executed when test is ok
-        if [ -e ${SEMAFORO} ]
+        if curl -m 30 -s https://www.github.com/ > /dev/null
         then
-                # Code executed when semaphore file is present, you can change it to send a mail, or some other alert methods
-                rm ${SEMAFORO}
-                #echo OK
-                /usr/local/bin/telegram-notify --title "Alert: Il sito www.github.com ha ripreso a funzionare." --success --quiet
-        fi
-else
-        # Code executed when test fail
-        if [ ! -e ${SEMAFORO} ]
-        then
-                # Code executed when semaphore file is not present, you can change it to send a mail, or some other alert methods
-                touch ${SEMAFORO} 
-                #echo NO
-                /usr/local/bin/telegram-notify --title "Alert: Il sito www.github.com non funziona più." --error --quiet
+                # Code executed when test is ok
+                if [ -e ${SEMAFORO} ]
+                then
+                        # Code executed when semaphore file is present, you can change it to send a mail, or some other alert methods
+                        rm ${SEMAFORO}
+                        #echo OK
+                        /usr/local/bin/telegram-notify --title "Alert: Il sito www.github.com ha ripreso a funzionare." --success --quiet
+                fi
+        else
+                # Code executed when test fail
+                if [ ! -e ${SEMAFORO} ]
+                then
+                        # Code executed when semaphore file is not present, you can change it to send a mail, or some other alert methods
+                        touch ${SEMAFORO} 
+                        #echo NO
+                        /usr/local/bin/telegram-notify --title "Alert: Il sito www.github.com non funziona più." --error --quiet
+                fi
         fi
 fi
